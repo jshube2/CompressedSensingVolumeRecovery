875d4ba30e2de76b0d0b39be539303f7c5e365f7
Alycen Wiacek <awiacek1@jhu.edu>
2018-05-01 13:39:35 -0400

    update main and add skull CT

--- CS_Testbench/main.m
+++ CS_Testbench/main.m
@@ -4,9 +4,8 @@ clc;
 %-------------------------------------------------------------------------%
 %-------------------------EXPERIMENT PARAMETERS---------------------------%
 %-------------------------------------------------------------------------%
-%sparsity = [10 20 30 40 50 60 70 80 90];
-sparsity = [50];
-
+sparsity = [10 20 30 40 50 60 70 80 90];
+%sparsity = 50;
 
 %-------------------------------------------------------------------------%
 %-------------------------RANDOM VOXEL DELETION---------------------------%
@@ -21,69 +20,21 @@ for i = 1:length(sparsity)
     %sparse_volume = makeCorrupt(volume, sparsity(i));
     sparse_volume = makeSparse(volume, sparsity(i));
     %sparse_volume = makeSparsePatches(volume, sparsity(i), 4);
-    viewCrossSection(sparse_volume);
-    title('Sparse Volume');
+    %viewCrossSection(sparse_volume);
+    %title('Sparse Volume');
     %makeVolumeMovie(sparse_volume); % USE THIS TO SAVE 3D VOLUME TO VIDEO
     %-DO-RECOVERY-HERE-%
     [recovered_volume, recovered_error] = recoveryAlgorithm(double(sparse_volume),'ALM',0.1);
     %recovered_volume = sparse_volume;
-    viewCrossSection(recovered_volume);
-    title('Recovered Volume');
+    %viewCrossSection(recovered_volume);
+    %title('Recovered Volume');
     %------------------%
-    error(i) = immse(double(volume), recovered_volume);
+    error(i) = ssim(double(volume), recovered_volume);
 end
 
 figure;
 plot(sparsity,error,'o-');
 title('Algorithm A');
 xlabel('Percentage of Missing Data');
-ylabel('Mean Squared Error');
-xlim([0 100]);
-
-%-------------------------------------------------------------------------%
-%------------------------------(ALGORITHM B)------------------------------%
-%----------------------------------SVT------------------------------------%
-for i = 1:length(sparsity)
-    sparse_volume = makeSparse(volume, sparsity(i));
-    viewCrossSection(sparse_volume);
-    title('Sparse Volume');
-    %makeVolumeMovie(sparse_volume); % USE THIS TO SAVE 3D VOLUME TO VIDEO
-    %-DO-RECOVERY-HERE-%
-    [recovered_volume, recovered_error] = recoveryAlgorithm(double(sparse_volume),'SVT',0.1);
-    %recovered_volume = sparse_volume;
-    viewCrossSection(recovered_volume);
-    title('Recovered Volume');
-    %------------------%
-    error(i) = immse(double(volume), recovered_volume);
-end
-
-figure;
-plot(sparsity,error,'o-');
-title('Algorithm B');
-xlabel('Percentage of Missing Data');
-ylabel('Mean Squared Error');
-xlim([0 100]);
-
-%-------------------------------------------------------------------------%
-%------------------------------(ALGORITHM C)------------------------------%
-%----------------------------------APG------------------------------------%
-for i = 1:length(sparsity)
-    sparse_volume = makeSparse(volume, sparsity(i));
-    viewCrossSection(sparse_volume);
-    title('Sparse Volume');
-    %makeVolumeMovie(sparse_volume); % USE THIS TO SAVE 3D VOLUME TO VIDEO
-    %-DO-RECOVERY-HERE-%
-    [recovered_volume, recovered_error] = recoveryAlgorithm(double(sparse_volume),'APG',0.1);
-    %recovered_volume = sparse_volume;
-    viewCrossSection(recovered_volume);
-    title('Recovered Volume');
-    %------------------%
-    error(i) = immse(double(volume), recovered_volume);
-end
-
-figure;
-plot(sparsity,error,'o-');
-title('Algorithm B');
-xlabel('Percentage of Missing Data');
-ylabel('Mean Squared Error');
+ylabel('SSIM');
 xlim([0 100]);
