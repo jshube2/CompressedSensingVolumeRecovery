4684e0dcafc22653a7995c6735ea1a713a28a0a4
Andrew Mao <amao@jhu.edu>
2018-05-02 15:58:38 -0400

    add plots

--- CS_Testbench/freq_analysis.m
+++ CS_Testbench/freq_analysis.m
@@ -3,8 +3,6 @@
 clear;
 close all;
 
-%% Vertebra CT
-
 num_images = 37;
 C = 0.1:0.1:0.9;
 SSIMs = zeros(num_images, length(C));
@@ -47,24 +45,50 @@ for k = 1:num_images
 end
 toc;
 
+% results
 C = [0, C, 1];
 SSIMs = horzcat(zeros(num_images, 1), SSIMs, ones(num_images, 1));
 save('results\freq_analysis.mat', 'SSIMs', 'C');
 
+idx = sort(randperm(35, 3));
 figure; hold on;
 plot(C, SSIMs(1, :), 'LineWidth', 2);
 plot(C, SSIMs(2, :), 'LineWidth', 2);
-plot(C, SSIMs(3, :), 'LineWidth', 2);
-plot(C, SSIMs(15, :), 'LineWidth', 2);
-plot(C, SSIMs(30, :), 'LineWidth', 2);
-legend('Vertebra CT', 'Head CT', 'Head MRI 1', 'Head MRI 13', 'Head MRI 28', 'location', 'best');
+plot(C, SSIMs(idx(1), :), 'LineWidth', 2);
+plot(C, SSIMs(idx(2), :), 'LineWidth', 2);
+plot(C, SSIMs(idx(3), :), 'LineWidth', 2);
+legend('Vertebra CT', 'Head CT', ['Head MRI ', num2str(idx(1))], ...
+    ['Head MRI ', num2str(idx(2))], ['Head MRI ', num2str(idx(3))], 'location', 'best');
 xlabel('Nyquist cutoff');
 ylabel('SSIM');
 title('Fourier Frequency sparsity of images');
 set(gca, 'fontsize', 16);
 saveas(gcf, 'results\freq_analysis.png');
 
-%% MRI
+figure;
+subplot(2,3,1); imagesc(flipud(squeeze(volume(round(sz2(1)), :, :)))); colormap gray; colorbar; axis image; axis off; title('Sagittal');
+subplot(2,3,2); imagesc(squeeze(volume(:, round(sz2(2)), :))); colormap gray; colorbar; axis image; axis off; title('Coronal');
+subplot(2,3,3); imagesc(squeeze(volume(:, :, round(sz2(3))))); colormap gray; colorbar; axis image; axis off; title('Axial');
+subplot(2,3,4); imagesc(log(1+abs(squeeze(F(round(sz2(1)), :, :)))));
+    colormap gray; colorbar; axis image; xlabel('f_y'); ylabel('f_z'); set(gca, 'XTick', [], 'YTick', []);
+subplot(2,3,5); imagesc(log(1+abs(squeeze(F(:, round(sz2(2)), :)))));
+    colormap gray; colorbar; axis image; xlabel('f_x'); ylabel('f_z'); set(gca, 'XTick', [], 'YTick', []);
+subplot(2,3,6); imagesc(log(1+abs(squeeze(F(:, :, round(sz2(3)))))));
+    colormap gray; colorbar; axis image; xlabel('f_x'); ylabel('f_y'); set(gca, 'XTick', [], 'YTick', []);
+saveas(gcf, 'results\freq_MRI.png');
 
-
+v = load('data\Head CT\headCT.mat');
+volume = v.u;
+F = fftshift(fftn(volume));
+figure;
+subplot(2,3,1); imagesc(flipud(squeeze(volume(256, :, :)))); colormap gray; colorbar; axis image; axis off; title('Sagittal');
+subplot(2,3,2); imagesc(squeeze(volume(:, 256, :))); colormap gray; colorbar; axis image; axis off; title('Coronal');
+subplot(2,3,3); imagesc(squeeze(volume(:, :, 90))); colormap gray; colorbar; axis image; axis off; title('Axial');
+subplot(2,3,4); imagesc(log(1+abs(squeeze(F(256, :, :)))));
+    colormap gray; colorbar; axis image; xlabel('f_y'); ylabel('f_z'); set(gca, 'XTick', [], 'YTick', []);
+subplot(2,3,5); imagesc(log(1+abs(squeeze(F(:, 256, :)))));
+    colormap gray; colorbar; axis image; xlabel('f_x'); ylabel('f_z'); set(gca, 'XTick', [], 'YTick', []);
+subplot(2,3,6); imagesc(log(1+abs(squeeze(F(:, :, 90)))));
+    colormap gray; colorbar; axis image; xlabel('f_x'); ylabel('f_y'); set(gca, 'XTick', [], 'YTick', []);
+saveas(gcf, 'results\freq_headCT.png');
 
